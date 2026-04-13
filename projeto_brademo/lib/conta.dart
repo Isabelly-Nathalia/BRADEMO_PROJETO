0be036bd0db9fe53fba8563c84c83d8d742f635a
Isabelly Nathalia <isabelly.nathalia.if@gmail.com>
2026-04-13 15:33:34 -0300

    tela básica de mapa e conta

--- projeto_brademo/lib/conta.dart
+++ projeto_brademo/lib/conta.dart
@@ -1,10 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:projeto_brademo/mapa.dart';
+import 'package:projeto_brademo/form.dart';
+import 'package:projeto_brademo/home.dart';
+import 'package:projeto_brademo/widgets/headerRotas.dart';
 
 class Conta extends StatelessWidget {
   const Conta({super.key});
 
-  @override
+@override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: const Color(0xFF222425),
+
+      appBar: AppBar(
+        title: const Text("Conta"),
+        backgroundColor: const Color(0xFF222425),
+        foregroundColor: Colors.white,
+      ),
+
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: Container(
+                width: double.infinity,
+                color: const Color(0xFF222425),
+              ),
+            ),
+
+            MenuWidget(
+              onHome: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Home()),
+                );
+              },
+              onForm: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => const SecondScreen()),
+                );
+              },
+              onMapa: () {},
+              onConta: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Conta()),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
